00799284001dbf2b9d65e9d3aa25ecd2eb5f221d
Sarah Dempsey <sarahmdempsey@live.com>
2016-09-05 20:29:04 -0700

    added user avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  	# Include default devise modules. Others available are:
+  	# :confirmable, :lockable, :timeoutable and :omniauthable
   	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -11,7 +11,9 @@ class User < ActiveRecord::Base
                      	:if => :password,
                      	:format => {:with => /\A.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A=Z])(?=.*[\@\#\$\%\^\&\+\=\!\*]).*\Z/}
 
-
+    has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100$#" }, :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+    
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904043239) do
+ActiveRecord::Schema.define(version: 20160906031356) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20160904043239) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

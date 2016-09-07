2b575909121fe50f49fc1e3f0f4fbc64a2ed92e5
Sarah Dempsey <sarahmdempsey@live.com>
2016-09-06 23:41:15 -0700

    fixed pin transition bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,7 @@ class User < ActiveRecord::Base
 
   	has_many :pins
 
-  	validates :password, :name, 	:presence => true,
-                     	:on => :create,
-                     	:if => :password,
-                     	:format => {:with => /\A.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A=Z])(?=.*[\@\#\$\%\^\&\+\=\!\*]).*\Z/}
+  	validates :password,  :presence => true, length: { minimum: 8 }
 
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100$#" }, :default_url => "/images/:style/missing.png"
     validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
